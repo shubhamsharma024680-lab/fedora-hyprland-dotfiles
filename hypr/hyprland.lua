f7783e9590623c072c1b76591e91d248e74bf6c0
Shubham Sharma <shubhamsharma024680@gmail.com>
2026-09-20 08:30:34 +0530

    decreased font size of cursor and kitty

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -69,8 +69,8 @@ end)
 
 -- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Environment-variables/
 
-hl.env("XCURSOR_SIZE", "24")
-hl.env("HYPRCURSOR_SIZE", "24")
+hl.env("XCURSOR_SIZE", "20")
+hl.env("HYPRCURSOR_SIZE", "20")
 
 
 -----------------------
